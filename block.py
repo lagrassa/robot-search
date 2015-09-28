820b5de83721bad17348917d409b49625a9c7605
Alex LaGrassa <lagrassa@mit.edu>
2015-09-28 16:48:22 -0400

    made block an obstacle object

--- block.py
+++ block.py
@@ -1,8 +1,9 @@
 import robot
 import polygon
+import obstacle
 
 other_vertex_list = [(0,50), (50,50), (50,0)]
 reference_point = (200,200)
 square = polygon.Polygon(reference_point, other_vertex_list)
 
-block = robot.Robot([square], reference_point)
+block = obstacle.Obstacle([square])
--- obstacle.py
+++ obstacle.py
@@ -3,5 +3,5 @@ import physical_object
 class Obstacle(physical_object.Physical_Object):
     def __init__(self, polygon_list):
         self.polygon_list = polygon_list
-        super().__init__()
+        physical_object.Physical_Object.__init__(self, polygon_list)
 
