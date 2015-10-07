3434053ff2848b69b48ba6824f53a9eab87700d1
Alex LaGrassa <lagrassa@mit.edu>
2015-10-07 15:34:49 -0400

    made block more moveable

--- block.py
+++ block.py
@@ -2,8 +2,9 @@ import robot
 import polygon
 import obstacle
 
-other_vertex_list = [(0,50), (50,50), (50,0)]
-reference_point = (200,200)
-square = polygon.Polygon(reference_point, other_vertex_list)
 
-block = obstacle.Obstacle([square])
+def block(reference_point):
+    other_vertex_list = [(0,50), (50,50), (50,0)]
+    square = polygon.Polygon(reference_point, other_vertex_list)
+    block = obstacle.Obstacle([square])
+    return block
--- main.py
+++ main.py
@@ -5,14 +5,10 @@ import environment
 
 other_vertex_list = [(0,50), (50,50), (50,0)]
 reference_point = (50,50)
-square = polygon.Polygon(reference_point, other_vertex_list)
-triangle_reference_point = (reference_point[0], reference_point[1]+50)
-triangle_other_vertex_list = [(0,50),(50,50)]
-triangle = polygon.Polygon(triangle_reference_point, triangle_other_vertex_list)
-triangle.color = (0, 0, 255)
 becky = becky.create_becky(reference_point)
-block = block.block
+block = block.block((200,200))
 resolution = 10
-algorithm = "BFS"
+#Can be BFS or DFS for blind_search, and "hill climbing" or "a star" for cost_based_search
+algorithm = "hill climbing"
 print "main start ", reference_point
 basic_game = environment.Environment(reference_point, (300,300), [block], becky, resolution, algorithm)
