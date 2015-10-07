8483b3d81863c018710f0202da6c32460844a2e9
Alex LaGrassa <lagrassa@mit.edu>
2015-10-07 15:28:44 -0400

    gave becky a hat

--- becky.py
+++ becky.py
@@ -1,3 +1,17 @@
+'''import robot
+import polygon
+
+def create_becky(reference_point):
+    other_vertex_list = [(0,50), (50,50), (50,0)]
+    square = polygon.Polygon(reference_point, other_vertex_list)
+    triangle_reference_point = (reference_point[0], reference_point[1]+50)
+    triangle_other_vertex_list = [(0,50),(50,50)],
+    triangle = polygon.Polygon(triangle_reference_point, triangle_other_vertex_list)
+    triangle.color = (0, 0, 255)
+    other_polygon.color = (35, 75, 0)
+    becky = robot.Robot([square,triangle], reference_point)
+    return becky
+'''
 import robot
 import polygon
 
@@ -7,8 +21,11 @@ def create_becky(reference_point):
     triangle_reference_point = (reference_point[0], reference_point[1]+50)
     triangle_other_vertex_list = [(0,50),(50,50)]
     triangle = polygon.Polygon(triangle_reference_point, triangle_other_vertex_list)
+    other_polygon_reference_point = (reference_point[0], reference_point[1]-50)
+    other_polygon_vertex_list = [(0,50),(50,50), (80,50), (50, 35)]
+    other_polygon = polygon.Polygon(other_polygon_reference_point, other_polygon_vertex_list)
     triangle.color = (0, 0, 255)
-    becky = robot.Robot([square, triangle], reference_point)
-    becky = robot.Robot([triangle], reference_point)
+    other_polygon.color = (255, 204, 255)
+    becky = robot.Robot([square, other_polygon, triangle], reference_point)
     return becky
 
