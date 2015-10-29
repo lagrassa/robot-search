c2d9e69a1a0fdd9deca92008b5378a07e7e96531
Alex LaGrassa <lagrassa@mit.edu>
2015-10-29 10:49:56 -0400

    Stops when finds path

--- search_lib.py
+++ search_lib.py
@@ -2,6 +2,7 @@ class Node:
     def __init__(self, state, parent = None):
         self.state = state
         self.parent = parent
+        self.child = None
         if self.parent is not None:
             self.path = tuple(list(self.parent.path)+[self.state])
         else:
