7a2b8b0350ceeeeead201548adabaefe5591d4e0
Alexandra L LaGrassa <lagrassa@mit.edu>
2015-11-03 16:19:57 -0500

    implemented some lib functions for decisions and tested it with an example

--- decisions/information_lib.py
+++ decisions/information_lib.py
@@ -1,37 +1,53 @@
 
+class Variable:
+    def __init__(self, name, values):
+        self.name = name
+        self.possible_values = values
+        self.evidence = {}
+
+
+
+
 #Returns the value of the current best action 
 def current_best_action(initial_evidence, utility_function, actions, possible_next_states):
     information_values_for_actions = []
     for action in actions:
-        givens = [initial_evidence]
+        givens = initial_evidence
         action_value = get_action_value(givens, action, utility_function, possible_next_states)
-        information_values_for_actions.append(action_value,action)
-    best_action = max(information_values_for_actions)
-    return best_action
+        information_values_for_actions.append((action_value,action))
+    print "Information value for actions", information_values_for_actions
+    best_action_tuple = max(information_values_for_actions)
+    best_action_value = best_action_tuple[0]
+    print "best_action_value", best_action_value
+    return best_action_value
 
 
-#Returns the value of the next best action 
-def new_best_action_given_e(initial_evidence, utility_function, actions, possible_next_states, new_evidence):
-    information_values_for_actions = []
-    for action in actions:
-        givens = [initial_evidence, new_evidence]
-        action_value = get_action_value(givens, action, utility_function, possible_next_states)
-        information_values_for_actions.append(action_value,action)
-    best_action = max(information_values_for_actions)
 
-def value_of_perfect_information(variable, initial_evidence, new_evidence, utility_function, actions, possible_next_states):
+def value_of_perfect_information(variable, initial_evidence, new_evidence_set, utility_function, actions, possible_next_states):
     possible_values = variable.possible_values
     value_of_new_information = 0
-    for possible_value in possible_values:
-        new_piece_of_evidence = new_evidence[possible_value]
-        value_of_new_information += new_best_action_given_e(initial_evidence,new_piece_of_evidence, utility_function, actions, possible_next_states, possible_value) 
+    for new_evidence in new_evidence_set:
+        value_of_evidence = current_best_action(new_evidence, utility_function, actions, possible_next_states) 
+        value_of_new_information += value_of_evidence
 
-    value_of_perfect_information = value_of_new_information - current_best_action(initial_evidence, utility_function, actions, possible_next_states)
+    value_of_old_information = current_best_action(initial_evidence, utility_function, actions, possible_next_states)
+    print value_of_old_information
+    value_of_perfect_information = value_of_new_information - value_of_old_information 
+   
     return value_of_perfect_information
 
+def get_action_value(givens, action, utility_function, possible_next_states):
+    values_for_states = 0
+    for state in possible_next_states:
+        state_value = probability_given_evidence(action, state, givens) * utility_function(state)
+        values_for_states += state_value
+    return values_for_states
 
-def probability_given_evidence():
-    pass
+def probability_given_evidence(action, possible_state, givens):
+    probability_result_equals_state = givens[action][possible_state]
+    return probability_result_equals_state
+    
+ 
 
 
 #current_rep:
@@ -53,18 +69,13 @@ def utility_function(state):
     utility = distance
     return utility
 
-initial_evidence = {straight_highway:0.9, winding_route:0.1 }
-new_evidence = {straight_highway:0.99, winding_route:0.01}
+initial_evidence = {straight_highway:{(100,100):0.9, (500,500):0.1}, winding_route:{(100,100):0.5, (500,500):0.5} }
+new_evidence = {straight_highway:{(100,100):0.99, (500,500):0.01}, winding_route:{(100,100):0.2, (500,500):0.7} }
 
 highway_to_take = Variable("highway", [straight_highway, winding_route]) 
 highway_to_take.evidence = initial_evidence
 
 possible_next_states = [(100,100),(500,500)]
-print value_of_perfect_information(variable, initial_evidence,new_evidence, utility_function, actions, possible_next_states)
-class Variable:
-    def __init__(self, values):
-        self.possible_values = values
-        self.evidence = {}
-
-
+new_evidence_set = [new_evidence]
+print value_of_perfect_information(highway_to_take, initial_evidence,new_evidence_set, utility_function, actions, possible_next_states)
 
