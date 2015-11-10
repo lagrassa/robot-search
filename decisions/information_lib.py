1e6efb6edb22850a0d7ac6319bea8ac51eeedb11
Alexandra L LaGrassa <lagrassa@mit.edu>
2015-11-10 16:11:52 -0500

    started incorporating the givenness hierarchy

--- decisions/information_lib.py
+++ decisions/information_lib.py
@@ -15,10 +15,8 @@ def current_best_action(initial_evidence, utility_function, actions, possible_ne
         givens = initial_evidence
         action_value = get_action_value(givens, action, utility_function, possible_next_states)
         information_values_for_actions.append((action_value,action))
-    print "Information value for actions", information_values_for_actions
     best_action_tuple = max(information_values_for_actions)
     best_action_value = best_action_tuple[0]
-    print "best_action_value", best_action_value
     return best_action_value
 
 
@@ -31,7 +29,6 @@ def value_of_perfect_information(variable, initial_evidence, new_evidence_set, u
         value_of_new_information += value_of_evidence
 
     value_of_old_information = current_best_action(initial_evidence, utility_function, actions, possible_next_states)
-    print value_of_old_information
     value_of_perfect_information = value_of_new_information - value_of_old_information 
    
     return value_of_perfect_information
@@ -48,34 +45,4 @@ def probability_given_evidence(action, possible_state, givens):
     return probability_result_equals_state
     
  
-
-
-#current_rep:
-#dictionary mapping states to probability
-
-def straight_highway(state):
-    expected_value = (100,100)
-    return expected_value
-
-def winding_route(state):
-    expected_value = (500,500)
-    return expected_value
-
-actions = [straight_highway, winding_route]
-
-def utility_function(state):
-    goal = (100,100)
-    distance = ((state[0]-goal[0])**2+(state[1]-goal[1])**2)**0.5
-    utility = distance
-    return utility
-
-initial_evidence = {straight_highway:{(100,100):0.9, (500,500):0.1}, winding_route:{(100,100):0.5, (500,500):0.5} }
-new_evidence = {straight_highway:{(100,100):0.99, (500,500):0.01}, winding_route:{(100,100):0.2, (500,500):0.7} }
-
-highway_to_take = Variable("highway", [straight_highway, winding_route]) 
-highway_to_take.evidence = initial_evidence
-
-possible_next_states = [(100,100),(500,500)]
-new_evidence_set = [new_evidence]
-print value_of_perfect_information(highway_to_take, initial_evidence,new_evidence_set, utility_function, actions, possible_next_states)
 
--- decisions/object.py
+++ decisions/object.py
@@ -42,24 +42,60 @@ def tokenize(sentence):
 #2. The most likely object
 #In this scenario, object #1 is a green ball, object #2 is a red ball, object #3 is an orange carrot
 #Based on the robots sensing capabilities, it has determined that 1 
-priors = {1:{"green ball": 0.6, "red ball":0.3, "orange carrot":0.1}, 2:{"green ball": 0.3, "red ball":0.6, "orange carrot":0.1}, 3:{"green ball": 0.1, "red ball":0.1, "orange carrot":0.9}}
+priors = {1:{green_ball: 0.6, red_ball:0.3, orange_carrot:0.1}, 2:{green_ball: 0.3, red_ball:0.6, orange_carrot:0.1}, 3:{green_ball: 0.1, red_ball:0.1, orange_carrot:0.9}}
 
-activated = ["green ball", "orange carrot"]
-LTM =  ["green ball", "orange carrot", "red ball"]
+activated = [green_ball, orange_carrot]
+LTM =  [green_ball, orange_carrot, red_ball]
 
 #To parse the statement, the modifier should have the following effects:
 #If the object has the modifier "this", then the object is in working memory (activated). Then, your given is that it is modified. Then, for state in distribution, find P(state)=object |object in activated 
-def distribution_given_level(distribution, level):
-    p_total_in_level= 0
+def distribution_given_set(distribution, narrowing_set):
+    p_total_in_set= 0
     updated_distribution = {}
     for state in distribution:
-        if state in level: 
-            p_total_in_level += distribution[state] 
-    
+        if state in narrowing_set: 
+            p_total_in_set += distribution[state] 
     for state in distribution:
         if state in level:
-            updated_distribution[state] = distribution[state]/(float(p_total_in_level))
+            updated_distribution[state] = distribution[state]/(float(p_total_in_set))
         else:
             updated_distribution[state]= 0
     return updated_distribution
+
+    
+
+def most_probable_object_given_sentence(sentence, activated, LTM, prior):
+    [determiner, modifier, subject] = tokenize(sentence)
+    distribution = prior
+    #improve distribution based on information given in the sentence
+    #start with the modifier
+    set_to_search = LTM
+    if determiner == "this":
+       distribution = distribution_given_set(prior,activated)
+       set_to_search = new_set
+   #narrows down based on color
+    modifier_set = []
+    for obj in set_to_search:
+        if obj.color == modifier:
+            modifier_set.append(obj) 
+    distribution = distribution_given_set(distribution, modifier_set)
+    #and object 
+    subject_set = []
+    for obj in set_to_search:
+        if obj.name == subject:
+            subject_set.append(obj) 
+    #and returns the object with the highest probability of being that.
+    max_prob = 0
+    object_number = None
+    for objectNum in distribution:
+        prob_of_object = distribution[objectNum][object] #todo make object
+        if prob_of_object > max_prob:
+            object_number = objectNum
+            max_prob = prob_of_object
+    return object_number 
+       
+   
+   #narrows down based on type
+   
+       
 print distribution_given_level(priors[1], activated)
