666d31fee38fd4a235fa5ba19373a19adc718886
Alexandra L LaGrassa <lagrassa@mit.edu>
2015-11-16 15:10:06 -0500

    implemented executable most probable object given sentence

--- decisions/object.py
+++ decisions/object.py
@@ -6,15 +6,14 @@ class Object:
         self.feature_vector = feature_vector
         self.location = None
     def __str__(self):
-        return "Givenness: " + self.givenness + " Features: " + str(self.feature_vector)
-
+        return str(self.feature_vector)
 
 class FeatureVector:
     def __init__(self, color, name):
         self.color = color
         self.name = name
     def __str__(self):
-        return "Color:"+ self.color + " Name: " + self.name
+        return str(self.color) + " " + str(self.name)
 
 #returns a list of the objects with their givenness
 def make_object(sentence):
@@ -22,8 +21,8 @@ def make_object(sentence):
     determiner = token[0]
     modifier = token[1]
     subject = token[2]
-    feature_vector = FeatureVector(modifier, subject)
-    return Object(determiner, feature_vector)
+    color = FeatureVector(modifier, subject)
+    return Object(determiner, color)
 
 def tokenize(sentence):
     determine_index = sentence.index(" ")
@@ -42,6 +41,10 @@ def tokenize(sentence):
 #2. The most likely object
 #In this scenario, object #1 is a green ball, object #2 is a red ball, object #3 is an orange carrot
 #Based on the robots sensing capabilities, it has determined that 1 
+green_ball = make_object("that green ball")
+red_ball = make_object("that red ball")
+orange_carrot = make_object("that orange carrot")
+
 priors = {1:{green_ball: 0.6, red_ball:0.3, orange_carrot:0.1}, 2:{green_ball: 0.3, red_ball:0.6, orange_carrot:0.1}, 3:{green_ball: 0.1, red_ball:0.1, orange_carrot:0.9}}
 
 activated = [green_ball, orange_carrot]
@@ -49,19 +52,29 @@ LTM =  [green_ball, orange_carrot, red_ball]
 
 #To parse the statement, the modifier should have the following effects:
 #If the object has the modifier "this", then the object is in working memory (activated). Then, your given is that it is modified. Then, for state in distribution, find P(state)=object |object in activated 
-def distribution_given_set(distribution, narrowing_set):
+def distribution_given_set_one_object(distribution, narrowing_set):
     p_total_in_set= 0
     updated_distribution = {}
     for state in distribution:
         if state in narrowing_set: 
             p_total_in_set += distribution[state] 
     for state in distribution:
-        if state in level:
+        if state in narrowing_set:
             updated_distribution[state] = distribution[state]/(float(p_total_in_set))
         else:
             updated_distribution[state]= 0
     return updated_distribution
 
+def distribution_given_set(distribution, narrowing_set):
+    updated_distribution = {}
+    for object_num in distribution:
+        object_distribution = distribution[object_num]
+        updated_distribution[object_num] = distribution_given_set_one_object(object_distribution, narrowing_set)
+    return updated_distribution
+    
+def most_probable_state(distribution):
+    most_probable_state = max(distribution, key = lambda x: distribution[x])
+    return most_probable_state
     
 
 def most_probable_object_given_sentence(sentence, activated, LTM, prior):
@@ -76,19 +89,20 @@ def most_probable_object_given_sentence(sentence, activated, LTM, prior):
    #narrows down based on color
     modifier_set = []
     for obj in set_to_search:
-        if obj.color == modifier:
+        if obj.feature_vector.color == modifier:
             modifier_set.append(obj) 
     distribution = distribution_given_set(distribution, modifier_set)
     #and object 
     subject_set = []
     for obj in set_to_search:
-        if obj.name == subject:
+        if obj.feature_vector.name == subject:
             subject_set.append(obj) 
     #and returns the object with the highest probability of being that.
     max_prob = 0
     object_number = None
     for objectNum in distribution:
-        prob_of_object = distribution[objectNum][object] #todo make object
+        most_probable_object_of_objectNum = most_probable_state(distribution[objectNum])
+        prob_of_object = distribution[objectNum][most_probable_object_of_objectNum]
         if prob_of_object > max_prob:
             object_number = objectNum
             max_prob = prob_of_object
@@ -97,5 +111,6 @@ def most_probable_object_given_sentence(sentence, activated, LTM, prior):
    
    #narrows down based on type
    
-       
-print distribution_given_level(priors[1], activated)
+sentence = "that green ball" 
+prior = priors
+print most_probable_object_given_sentence(sentence, activated, LTM, prior)
